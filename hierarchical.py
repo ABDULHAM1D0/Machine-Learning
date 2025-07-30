cd56c27f31c4efe2608839b8f7a442086448f3f6
Abduhamid <133525851+ABDULHAM1D0@users.noreply.github.com>
2025-07-31 00:14:42 +0300

    Update hierarchical.py

--- hierarchical.py
+++ hierarchical.py
@@ -8,3 +8,24 @@ X, _ = make_blobs(n_samples=300, centers = 4, cluster_std=1.2, random_state=42)
 plt.figure()
 plt.scatter(X[:, 0], X[:, 1])
 plt.title("Sample dataset")
+
+linkage_methods = ["ward", "single", "average", "complete"]
+
+plt.figure()
+for i, linkage_method in enumerate(linkage_methods, 1):
+    
+    model = AgglomerativeClustering(n_clusters=4, linkage=linkage_method)
+    label = model.fit_predict(X)
+    
+    plt.subplot(2, 4, i)
+    plt.title(f"{linkage_method.capitalize()} linkage dendrogram")
+    dendrogram(linkage(X, method=linkage_method), no_labels=True)
+    plt.xlabel("Data points")
+    plt.ylabel("Distance")
+    
+    plt.subplot(2, 4, i + 4)
+    plt.scatter(X[:, 0], X[:, 1], c = label, cmap = "viridis")
+    plt.title(f"{linkage_method.capitalize()} linkage dendrogram")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    
